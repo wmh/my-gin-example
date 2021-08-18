c4406093daf4b13e65d204fd3ca82784511d1415
Hunter Wu <hunter.wu@gmail.com>
2021-08-18 23:43:48 +0800

    Add ReverseProxy example

--- app/routes/example.go
+++ app/routes/example.go
@@ -1,6 +1,10 @@
 package routes
 
 import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
 	"github.com/gin-gonic/gin"
 	"github.com/wmh/my-gin-example/app/controllers"
 	"github.com/wmh/my-gin-example/app/services"
@@ -20,4 +24,22 @@ func MakeExampleAPI(r *gin.Engine) {
 			exampleAuth.GET("", controllers.AuthHello)
 		}
 	}
+
+	r.Any("/proxypass/*proxyPath", proxy)
+}
+
+func proxy(c *gin.Context) {
+	remote, err := url.Parse("http://localhost:8010")
+	if err != nil {
+		panic(err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy.Director = func(req *http.Request) {
+		req.Header = c.Request.Header
+		req.Host = remote.Host
+		req.URL.Scheme = remote.Scheme
+		req.URL.Host = remote.Host
+		req.URL.Path = c.Param("proxyPath")
+	}
+	proxy.ServeHTTP(c.Writer, c.Request)
 }
